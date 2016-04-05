328e94fc53ca4ce56befa9a288773fb5681545a9
Douglas Camata <douglas.camata@nexedi.com>
2016-04-05 20:34:41 +0200

    wendelin_configurator: add default knowledge pad with wendelin info gadget
    Now activates the knowledge pad homepage and create an instance of Wendelin Info
    gadget automatically inside it.

--- bt5/erp5_wendelin_configurator/SkinTemplateItem/portal_skins/erp5_configurator_wendelin/BusinessConfiguration_afterWendelinConfiguration.py
+++ bt5/erp5_wendelin_configurator/SkinTemplateItem/portal_skins/erp5_configurator_wendelin/BusinessConfiguration_afterWendelinConfiguration.py
@@ -2,4 +2,8 @@
 This script will be called to apply the customization. 
 """
 
-# Ivan: we simply do nothing for now
+# Activate the knowledge pads on portal home to enable later the Wendelin 
+# Information gadget.
+#
+configuration = context.restrictedTraverse('portal_preferences/default_site_preference')
+configuration.setPreferredHtmlStyleAccessTab(True)
