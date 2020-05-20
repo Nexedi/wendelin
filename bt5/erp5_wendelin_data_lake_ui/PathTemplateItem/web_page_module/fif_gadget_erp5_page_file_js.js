646c9c28679740ae51fcae002465fdd84b46c227
Roque Porchetto <roque.porchetto@nexedi.com>
2020-05-20 14:50:48 +0000

    erp5_wendelin_data_lake_ui: update renamed urls

--- bt5/erp5_wendelin_data_lake_ui/PathTemplateItem/web_page_module/fif_gadget_erp5_page_file_js.js
+++ bt5/erp5_wendelin_data_lake_ui/PathTemplateItem/web_page_module/fif_gadget_erp5_page_file_js.js
@@ -112,7 +112,7 @@
         });
     })
     .declareMethod("getDescriptorContent", function (descriptorReference) {
-      var url = "/erp5/ERP5_getDescriptorHTMLContent?reference=" + descriptorReference,
+      var url = "/ERP5_getDescriptorHTMLContent?reference=" + descriptorReference,
           xmlHttp = new XMLHttpRequest();
       try {
           xmlHttp.open("GET", url, false);
