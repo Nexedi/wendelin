fddfc0953cee1d04679333787704a2c0a736df95
Roque Porchetto <roque.porchetto@nexedi.com>
2020-06-08 13:07:14 +0000

    erp5_wendelin_data_lake_ingestion: return new datastream values as a list

--- bt5/erp5_wendelin_data_lake_ingestion/SkinTemplateItem/portal_skins/erp5_wendelin_data_lake/ERP5Site_getDataStreamList.py
+++ bt5/erp5_wendelin_data_lake_ingestion/SkinTemplateItem/portal_skins/erp5_wendelin_data_lake/ERP5Site_getDataStreamList.py
@@ -10,7 +10,7 @@ portal = context.getPortalObject()
 try:
   data_set = portal.data_set_module.get(data_set_reference)
   if data_set is None or portal.ERP5Site_checkReferenceInvalidated(data_set):
-    return { "status_code": 0, "result": [] }
+    return { "status_code": 0, "result": [], 'result_dict': [] }
 except Exception as e: # fails because unauthorized access
   log("Unauthorized access to getDataStreamList: " + str(e))
   return { "status_code": 1, "error_message": "401 - Unauthorized access. Please check your user credentials and try again." }
@@ -36,8 +36,10 @@ for stream in data_set.DataSet_getDataStreamList():
       data_stream_dict[stream.getReference()]['full-size'] = int(data_stream_dict[stream.getReference()]['full-size']) + int(stream.getSize())
     else:
       data_stream_dict[stream.getReference()] = { 'data-stream-list': [data_stream_info_dict],
+                                                        'id': 'data_stream_module/'+stream.getId(),
+                                                        'reference': stream.getReference(),
                                                         'large-hash': stream.getVersion(),
                                                         'full-size': stream.getSize() }
 
-result_dict = { 'status_code': 0, 'result': data_stream_list, 'result_dict': data_stream_dict}
+result_dict = { 'status_code': 0, 'result': data_stream_list, 'result_dict': data_stream_dict.values()}
 return json.dumps(result_dict)
