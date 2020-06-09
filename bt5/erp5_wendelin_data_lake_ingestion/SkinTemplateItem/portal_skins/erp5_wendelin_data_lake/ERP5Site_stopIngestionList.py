645ef1c9b4e61ce17fcc01d582601a9883c82fde
Roque Porchetto <roque.porchetto@nexedi.com>
2020-06-09 14:01:21 +0000

    erp5_wendelin_data_lake_ingestion: add state control during data stream validation

--- bt5/erp5_wendelin_data_lake_ingestion/SkinTemplateItem/portal_skins/erp5_wendelin_data_lake/ERP5Site_stopIngestionList.py
+++ bt5/erp5_wendelin_data_lake_ingestion/SkinTemplateItem/portal_skins/erp5_wendelin_data_lake/ERP5Site_stopIngestionList.py
@@ -68,7 +68,7 @@ for data_ingestion in portal_catalog(portal_type = "Data Ingestion",
         if data_stream.getVersion() is None:
           hash_value = getHash(data_stream)
           data_stream.setVersion(hash_value)
-        if data_stream.getValidationState() != "validated":
+        if data_stream.getValidationState() != "validated" and data_stream.getValidationState() != "published":
           data_stream.validate()
         if data_stream.getValidationState() != "published":
           data_stream.publish()
