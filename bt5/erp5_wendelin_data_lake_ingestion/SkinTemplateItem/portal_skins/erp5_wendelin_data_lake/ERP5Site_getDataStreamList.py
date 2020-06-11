37933793bf8bf68a1eeade9bf1575f2e7bc0ab9b
Roque Porchetto <roque.porchetto@nexedi.com>
2020-06-11 13:39:06 +0000

    erp5_wendelin_data_lake_ingestion: getDataStreamList script cleanup

--- bt5/erp5_wendelin_data_lake_ingestion/SkinTemplateItem/portal_skins/erp5_wendelin_data_lake/ERP5Site_getDataStreamList.py
+++ bt5/erp5_wendelin_data_lake_ingestion/SkinTemplateItem/portal_skins/erp5_wendelin_data_lake/ERP5Site_getDataStreamList.py
@@ -15,10 +15,6 @@ except Exception as e: # fails because unauthorized access
   log("Unauthorized access to getDataStreamList: " + str(e))
   return { "status_code": 1, "error_message": "401 - Unauthorized access. Please check your user credentials and try again." }
 
-data_set = portal.data_set_module.get(data_set_reference)
-if data_set is None:
-  return []
-
 data_stream_dict = {}
 for stream in data_set.DataSet_getDataStreamList():
   if not portal.ERP5Site_checkReferenceInvalidated(stream) and stream.getValidationState() != "draft":
@@ -31,10 +27,10 @@ for stream in data_set.DataSet_getDataStreamList():
       data_stream_dict[stream.getReference()]['full-size'] = int(data_stream_dict[stream.getReference()]['full-size']) + int(stream.getSize())
     else:
       data_stream_dict[stream.getReference()] = { 'data-stream-list': [data_stream_info_dict],
-                                                        'id': 'data_stream_module/'+stream.getId(),
-                                                        'reference': stream.getReference(),
-                                                        'large-hash': stream.getVersion(),
-                                                        'full-size': stream.getSize() }
+                                                  'id': 'data_stream_module/'+stream.getId(),
+                                                  'reference': stream.getReference(),
+                                                  'large-hash': stream.getVersion(),
+                                                  'full-size': stream.getSize() }
 
 result_dict = { 'status_code': 0, 'result': data_stream_dict.values()}
 return json.dumps(result_dict)
