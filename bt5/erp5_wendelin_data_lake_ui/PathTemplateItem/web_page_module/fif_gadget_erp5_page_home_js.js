be3c7cf0d0a1485cb57090202f0a24ed0d212e7f
Roque Porchetto <roque.porchetto@nexedi.com>
2020-06-25 19:32:54 +0200

    Revert "[WIP] erp5_wendelin_data_lake_ui: change home page urls"
    This reverts commit eeb0444cc2ae92067d8efd478707d8345cadd041.

--- bt5/erp5_wendelin_data_lake_ui/PathTemplateItem/web_page_module/fif_gadget_erp5_page_home_js.js
+++ bt5/erp5_wendelin_data_lake_ui/PathTemplateItem/web_page_module/fif_gadget_erp5_page_home_js.js
@@ -1,63 +1,35 @@
-/*global window, rJS, RSVP, URI, document */
+/*global window, rJS, RSVP, URI */
 /*jslint nomen: true, indent: 2, maxerr: 3 */
 (function (window, rJS, RSVP, document) {
   "use strict";
-  /*function data_lake(context, evt) {
+  function data_lake(context, evt) {
     var link = document.createElement('a');
     link.href = window.location.origin + "/erp5/web_site_module/fif_data_runner/#/?page=fifdata";
     link.click();
-  }*/
+  }
 
   rJS(window)
     .declareAcquiredMethod("updateHeader", "updateHeader")
-    .declareAcquiredMethod("getUrlForList", "getUrlForList")
-    /*.declareJob('data_lake', function (evt) {
+    .declareJob('data_lake', function (evt) {
       return data_lake(this, evt);
-    })*/
-    .declareMethod("render", function (options) {
-      return this.changeState(options);
     })
-    .onStateChange(function () {
+    .declareMethod("render", function () {
       var gadget = this;
-      return gadget.updateHeader({
-        page_title: 'Wendelin Data Lake Sharing Platform'
-      })
+      return new RSVP.Queue()
         .push(function () {
-          var url_parameter_list = [];
-          url_parameter_list.push({
-              command: 'display'
-            });
-          url_parameter_list.push({
-              command: 'display_stored_state',
-              options: {page: 'download'}
-            });
-          url_parameter_list.push({
-              command: 'display_stored_state',
-              options: {page: 'fifdata'}
-            });
-          url_parameter_list.push({
-              command: 'display_stored_state',
-              options: {page: 'register'}
-            });
-          url_parameter_list.push({
-              command: 'display_stored_state',
-              options: {page: 'ebulk_doc'}
-            });
-          return gadget.getUrlForList(url_parameter_list);
-        })
-        .push(function (url_list) {
-          console.log("url_list:", url_list);
-          console.log(document.querySelector("#register_link"));
+          return gadget.updateHeader({
+            page_title: 'Wendelin Data Lake Sharing Platform'
+          });
         })
         .push(undefined, function (error) {
           throw error;
         });
     })
-    /*.onEvent('submit', function (evt) {
+    .onEvent('submit', function (evt) {
       if (evt.target.name === 'data-lake') {
         return this.data_lake(evt);
       } else {
         throw new Error('Unknown form');
       }
-    })*/;
+    });
 }(window, rJS, RSVP, document));
