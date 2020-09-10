06658082e153ddf8a49dfa381c4d253f7cb2fbfa
Roque Porchetto <roque.porchetto@nexedi.com>
2020-09-10 18:03:56 +0000

    erp5_wendelin_data_lake_ingestion: fix pubish data set script

--- bt5/erp5_wendelin_data_lake_ingestion/WorkflowTemplateItem/portal_workflow/data_set_interaction_workflow/scripts/DataSet_publishDataStreamList.py
+++ bt5/erp5_wendelin_data_lake_ingestion/WorkflowTemplateItem/portal_workflow/data_set_interaction_workflow/scripts/DataSet_publishDataStreamList.py
@@ -3,8 +3,5 @@
 """
 data_set = state_change['object']
 for data_stream in data_set.DataSet_getDataStreamList():
-  try:
+  if data_stream and not context.getPortalObject().ERP5Site_checkReferenceInvalidated(data_stream) and data_stream.getValidationState() != 'draft':
     data_stream.activate().publish()
-  except:
-    #ignore draft data streams or corresponding to old invalidated ingestions
-    pass
