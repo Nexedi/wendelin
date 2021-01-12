b6840dcf360043df2f90cc8fbe2dc38bcf667b08
Ivan Tyagov <ivan@nexedi.com>
2021-01-12 15:30:32 +0200

    erp5_wendelin_data_lake_ingestion: catalog certain properties of a Data Stream for ebulk client side optimizations.

---
+++ bt5/erp5_wendelin_data_lake_ingestion/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/z_catalog_data_stream_list.sql
@@ -0,0 +1,12 @@
+REPLACE INTO
+  data_stream (uid, set_uid, size, version)
+VALUES
+<dtml-in prefix="loop" expr="_.range(_.len(uid))">
+(
+  <dtml-sqlvar expr="uid[loop_item]" type="int">,
+  <dtml-sqlvar expr="getSetUid[loop_item]" type="int" optional>,
+  <dtml-sqlvar expr="getSize[loop_item]" type="string" optional>,
+  <dtml-sqlvar expr="getVersion[loop_item]" type="string" optional>
+)
+<dtml-if sequence-end><dtml-else>,</dtml-if>
+</dtml-in>
