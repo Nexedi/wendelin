f9ade0fb02519810109145422f4ab7522a90db2a
Ivan Tyagov <ivan@nexedi.com>
2021-01-15 16:19:50 +0200

    Moved from erp5_wendelin.

---
+++ bt5/erp5_wendelin_data_lake_ingestion/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/z_create_data_stream.sql
@@ -0,0 +1,11 @@
+# Host:
+# Database: test
+# Table: 'data_stream'
+#
+CREATE TABLE `data_stream` (
+  `uid` BIGINT UNSIGNED NOT NULL,
+  `set_uid` BIGINT UNSIGNED,
+  `size` BIGINT SIGNED,
+  `version` varchar(30) default '',
+  PRIMARY KEY  (`uid`)
+) ENGINE=InnoDB;
