e2d05cf0fc74f22883929f607d38a7353e483b38
Ivan Tyagov <ivan@nexedi.com>
2021-01-19 14:32:26 +0200

    During ingestion the relation between Data Set and Data Stream is set on Data Ingestion Line. This seemed enough for small Data Sets (few thousands of Data Streams). With large Data Sets this is inefficient as getting list of Data Streams for a Data Set takes sometimes tens of minutes. Thus we needed a faster way to loop up: which is an explicitl relation over set category and proper MySQL indexing. This commit is a trade-off between cost of storage (i.e. a category) compared to scalability and performance of a system.

--- bt5/erp5_wendelin_data_lake_ingestion/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/z_catalog_data_stream_list.sql
+++ bt5/erp5_wendelin_data_lake_ingestion/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/z_catalog_data_stream_list.sql
@@ -4,7 +4,7 @@ VALUES
 <dtml-in prefix="loop" expr="_.range(_.len(uid))">
 (
   <dtml-sqlvar expr="uid[loop_item]" type="int">,
-  <dtml-sqlvar expr="DataStream_getSetUid[loop_item]" type="int" optional>,
+  <dtml-sqlvar expr="getSetUid[loop_item]" type="int" optional>,
   <dtml-sqlvar expr="getSize[loop_item]" type="string" optional>,
   <dtml-sqlvar expr="getVersion[loop_item]" type="string" optional>
 )
