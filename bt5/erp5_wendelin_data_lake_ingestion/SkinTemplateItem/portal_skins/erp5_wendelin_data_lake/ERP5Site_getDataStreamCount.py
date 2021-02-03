6be0823c71985d1963045108b5b92b6f60ba7fde
Roque Porchetto <roque.porchetto@nexedi.com>
2021-02-03 19:18:09 +0000

    erp5_wendelin_data_lake_ingestion: new way to get dataset information

---
+++ bt5/erp5_wendelin_data_lake_ingestion/SkinTemplateItem/portal_skins/erp5_wendelin_data_lake/ERP5Site_getDataStreamCount.py
@@ -0,0 +1,20 @@
+"""
+This script is called from ebulk client to get count of Data Streams for a Data set.
+"""
+from erp5.component.module.Log import log
+
+portal = context.getPortalObject()
+
+try:
+  data_set = portal.data_set_module.get(data_set_reference)
+  if data_set is None or data_set.getReference().endswith("_invalid"):
+    return { "status_code": 0, "result": 0 }
+except Exception as e: # fails because unauthorized access
+  log("Unauthorized access to getDataStreamList: " + str(e))
+  return { "status_code": 1, "error_message": "401 - Unauthorized access. Please check your user credentials and try again." }
+
+catalog_kw = dict(portal_type = "Data Stream",
+                  set_uid = data_set.getUid(),
+                  validation_state = ['published', 'validated'])
+data_stream_brain_list = portal.portal_catalog(**catalog_kw)
+return { "status_code": 0, "result": len(data_stream_brain_list) }
