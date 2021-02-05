bc0038afa1158d77d154549aa8ce34a8f0be7ed1
Roque Porchetto <roque.porchetto@nexedi.com>
2021-02-05 16:29:10 +0000

    erp5_wendelin_data_lake_ingestion: refactor get data stream script queries - new script to get data set number of files

--- bt5/erp5_wendelin_data_lake_ingestion/SkinTemplateItem/portal_skins/erp5_wendelin_data_lake/ERP5Site_getDataStreamList.py
+++ bt5/erp5_wendelin_data_lake_ingestion/SkinTemplateItem/portal_skins/erp5_wendelin_data_lake/ERP5Site_getDataStreamList.py
@@ -4,34 +4,41 @@ This script is called from ebulk client to get list of Data Streams for a Data s
 import json
 from erp5.component.module.Log import log
 
+limit=[]
+if batch_size:
+  limit=[offset, batch_size]
+
 portal = context.getPortalObject()
 
 try:
   data_set = portal.data_set_module.get(data_set_reference)
-  # XXX: why does we need reference= "something_invalidated" when we have Data Set's state ?
   if data_set is None or data_set.getReference().endswith("_invalid"):
-    return { "status_code": 0, "result": [] }
+    return json.dumps({ "status_code": 0, "result": [] })
 except Exception as e: # fails because unauthorized access
   log("Unauthorized access to getDataStreamList: " + str(e))
-  return { "status_code": 1, "error_message": "401 - Unauthorized access. Please check your user credentials and try again." }
+  return json.dumps({ "status_code": 1, "error_message": "401 - Unauthorized access. Please check your user credentials and try again." })
+
+data_set_uid = data_set.getUid()
+
+catalog_kw = {'portal_type': 'Data Ingestion Line',
+              'aggregate_uid': data_set_uid,
+              'limit': limit,
+              }
+data_ingestion_line_list = context.portal_catalog(**catalog_kw)
+data_ingestion_uid_list = [x.getUid() for x in data_ingestion_line_list]
+
+data_stream_list = context.getPortalObject().portal_catalog(
+  portal_type="Data Stream",
+  aggregate__related__uid=data_ingestion_uid_list,
+  select_list=['reference', 'relative_url', 'data_stream.size', 'data_stream.version'],
+)
 
 data_stream_dict = {}
-# XXX: reference NOT ending with "_invalidated" -> why is that needed when we can invalidate Data Stream ???
-# XXX: state != draft
-catalog_kw = dict(portal_type = "Data Stream", 
-                  set_uid = data_set.getUid(),
-                  #limit=[1000, 20], # XXX: add new script arguments start and offset to script and make ebulk use them!
-                  sort_on=(("creation_date", "ascending",),),
-                  validation_state = ['published', 'validated'])
-data_stream_brain_list = portal.portal_catalog(**catalog_kw)
-context.log("Data Streams found=%s" %len(data_stream_brain_list))
-
-for stream_brain in data_stream_brain_list:
-  reference = stream_brain.getReference()
+for stream_brain in data_stream_list:
+  reference = stream_brain.reference
   version = stream_brain.version
   size = stream_brain.size
-  data_stream_id = "data_stream_module/%s" %stream_brain.id
-  #context.log("id=%s, version=%s, reference=%s, size=%s" %(data_stream_id, version, reference, size))
+  data_stream_id = stream_brain.relative_url
   data_stream_info_dict = {'id': data_stream_id,
                            'size': size,
                            'hash': version}
