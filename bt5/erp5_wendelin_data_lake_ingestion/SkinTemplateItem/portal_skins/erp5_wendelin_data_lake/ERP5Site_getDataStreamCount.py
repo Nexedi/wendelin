27b6c0e45b78503a6aabe350dcab8e3ca0eafad7
Roque Porchetto <roque.porchetto@nexedi.com>
2021-02-05 16:29:10 +0000

    erp5_wendelin_data_lake_ingestion: refactor get data stream script queries - new script to get data set number of files

---
+++ bt5/erp5_wendelin_data_lake_ingestion/SkinTemplateItem/portal_skins/erp5_wendelin_data_lake/ERP5Site_getDataStreamCount.py
@@ -0,0 +1,29 @@
+"""
+This script is called from ebulk client to get count of Data Streams for a Data set.
+"""
+from erp5.component.module.Log import log
+
+portal = context.getPortalObject()
+
+try:
+  data_set = portal.data_set_module.get(data_set_reference)
+  if data_set is None or data_set.getReference().endswith("_invalid"):
+    return { "status_code": 0, "result": 0 }
+except Exception as e:
+  log("Unauthorized access to getDataStreamList: " + str(e))
+  return { "status_code": 1, "error_message": "401 - Unauthorized access. Please check your user credentials and try again." }
+
+data_set_uid = data_set.getUid()
+
+catalog_kw = {'portal_type': 'Data Ingestion Line',
+              'aggregate_uid': data_set_uid
+              }
+data_ingestion_line_list = context.portal_catalog(**catalog_kw)
+data_ingestion_uid_list = [x.getUid() for x in data_ingestion_line_list]
+
+data_stream_list = context.getPortalObject().portal_catalog(
+  portal_type="Data Stream",
+  aggregate__related__uid=data_ingestion_uid_list
+)
+
+return { "status_code": 0, "result": len(data_stream_list) }
