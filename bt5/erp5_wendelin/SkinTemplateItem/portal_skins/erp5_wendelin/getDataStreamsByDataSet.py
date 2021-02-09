785e010c0972a729450f312e4019c0391de53cbd
Roque Porchetto <roque.porchetto@nexedi.com>
2021-02-09 18:35:34 +0000

    erp5_wendelin: "Data Streams" tab in Data Set

---
+++ bt5/erp5_wendelin/SkinTemplateItem/portal_skins/erp5_wendelin/getDataStreamsByDataSet.py
@@ -0,0 +1,13 @@
+catalog_kw = {'portal_type': 'Data Ingestion Line',
+              'aggregate_uid': data_set_uid,
+              'limit': limit,
+              }
+data_ingestion_line_list = context.portal_catalog(**catalog_kw)
+if data_ingestion_line_list:
+  data_ingestion_uid_list = [x.getUid() for x in data_ingestion_line_list]
+  catalog_kw = {'portal_type': 'Data Stream',
+                'aggregate__related__uid': data_ingestion_uid_list,
+                'select_list': ['reference', 'relative_url', 'versioning.size', 'versioning.version'],
+                }
+  return context.getPortalObject().portal_catalog(**catalog_kw)
+return data_ingestion_line_list
