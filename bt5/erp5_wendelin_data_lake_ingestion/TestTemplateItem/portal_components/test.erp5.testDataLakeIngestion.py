af96cb2fde3d6e36e7a9728f42712276e9c0dda0
Roque Porchetto <roque.porchetto@nexedi.com>
2021-02-09 17:21:49 +0000

    erp5_wendelin_data_lake_ingestion: better control fix

--- bt5/erp5_wendelin_data_lake_ingestion/TestTemplateItem/portal_components/test.erp5.testDataLakeIngestion.py
+++ bt5/erp5_wendelin_data_lake_ingestion/TestTemplateItem/portal_components/test.erp5.testDataLakeIngestion.py
@@ -22,6 +22,7 @@ class TestDataIngestion(SecurityTestCase):
   REF_PREFIX = "fake-supplier" + REFERENCE_SEPARATOR
   REF_SUPPLIER_PREFIX = "fake-supplier" + REFERENCE_SEPARATOR
   INVALID = "_invalid"
+  REF_DATASET = "fake-dataset"
 
   def getTitle(self):
     return "DataIngestionTest"
@@ -38,7 +39,7 @@ class TestDataIngestion(SecurityTestCase):
 
   def getIngestionReference(self, reference, extension, randomize_ingestion_reference=False, data_set_reference=False):
     if not data_set_reference:
-      data_set_reference = "fake-dataset"
+      data_set_reference = self.REF_DATASET
     if not randomize_ingestion_reference:
       # return hard coded which results in one Data Set and multiple Data Streams (in context of test)
       return self.REF_PREFIX + data_set_reference + self.REFERENCE_SEPARATOR + reference + extension
@@ -311,7 +312,6 @@ class TestDataIngestion(SecurityTestCase):
       Try to ingest same element twice while previous is being processed
     """
     reference = self.getRandomReference()
-    reference = "reemplazable"
 
     self.ingest("some-data", reference, self.CSV, self.SINGLE_INGESTION_END)
     time.sleep(1)
@@ -321,8 +321,19 @@ class TestDataIngestion(SecurityTestCase):
     self.portal.portal_alarms.wendelin_handle_analysis.Alarm_handleAnalysis()
     self.tic()
 
+    ingestion_reference = self.REF_DATASET + self.REFERENCE_SEPARATOR + reference + self.CSV
+    data_ingestion = self.getDataIngestion(ingestion_reference)
+    data_stream = self.getDataStream(ingestion_reference)
+
+    self.assertEqual(data_ingestion.getSimulationState(), "stopped")
+    self.assertEqual(data_stream.getValidationState(), "validated")
+
+    # the new ingestion should replace the old one
     self.ingest("some-data", reference, self.CSV, self.SINGLE_INGESTION_END)
     time.sleep(1)
     self.tic()
+
+    self.assertEqual(data_stream.getValidationState(), "invalidated")
+    self.assertTrue(data_ingestion.getReference().endswith(self.INVALID))
 
   # XXX: new test which simulates download / upload of Data Set and increase DS version
