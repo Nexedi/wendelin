125ff40cfff1dff3ea4b2f71c1c3865ab531f474
Roque Porchetto <roque.porchetto@nexedi.com>
2021-02-23 17:15:52 +0000

    erp5_wendelin: common script and Data Streams tab update

--- bt5/erp5_wendelin/SkinTemplateItem/portal_skins/erp5_wendelin/DataSet_getDataStreamRowList.py
+++ bt5/erp5_wendelin/SkinTemplateItem/portal_skins/erp5_wendelin/DataSet_getDataStreamRowList.py
@@ -2,16 +2,18 @@
 Get list of Data Streams for context Data set.
 """
 data_set_uid = context.getUid()
-
-catalog_kw = {'portal_type': 'Data Ingestion Line',
+return context.getDataStreamsByDataSet(data_set_uid, limit)
+'''catalog_kw = {'portal_type': 'Data Ingestion Line',
               'aggregate_uid': data_set_uid,
               'limit': limit,
               }
 data_ingestion_line_list = context.portal_catalog(**catalog_kw)
-data_ingestion_uid_list = [x.getUid() for x in data_ingestion_line_list]
+if data_ingestion_line_list:
+  data_ingestion_uid_list = [x.getUid() for x in data_ingestion_line_list]
 
-return context.getPortalObject().portal_catalog(
-  portal_type="Data Stream",
-  aggregate__related__uid=data_ingestion_uid_list,
-  select_list=['reference', 'relative_url', 'data_stream.size', 'data_stream.version'],
-)
+  return context.getPortalObject().portal_catalog(
+    portal_type="Data Stream",
+    aggregate__related__uid=data_ingestion_uid_list,
+    select_list=['reference', 'relative_url', 'data_stream.size', 'data_stream.version'],
+  )
+return []'''
