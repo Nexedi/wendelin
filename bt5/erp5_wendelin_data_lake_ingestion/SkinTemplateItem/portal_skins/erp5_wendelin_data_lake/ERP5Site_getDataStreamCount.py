c1d06c63c9031bbe7cd0f074e5896b30d55a18dc
Roque Porchetto <roque.porchetto@nexedi.com>
2021-02-23 17:14:03 +0000

    erp5_wendelin_data_lake_ingestion: new scripts use common script

--- bt5/erp5_wendelin_data_lake_ingestion/SkinTemplateItem/portal_skins/erp5_wendelin_data_lake/ERP5Site_getDataStreamCount.py
+++ bt5/erp5_wendelin_data_lake_ingestion/SkinTemplateItem/portal_skins/erp5_wendelin_data_lake/ERP5Site_getDataStreamCount.py
@@ -14,8 +14,9 @@ except Exception as e:
   return { "status_code": 1, "error_message": "401 - Unauthorized access. Please check your user credentials and try again." }
 
 data_set_uid = data_set.getUid()
+data_stream_list = context.getDataStreamsByDataSet(data_set_uid)
 
-catalog_kw = {'portal_type': 'Data Ingestion Line',
+'''catalog_kw = {'portal_type': 'Data Ingestion Line',
               'aggregate_uid': data_set_uid
               }
 data_ingestion_line_list = context.portal_catalog(**catalog_kw)
@@ -24,6 +25,6 @@ data_ingestion_uid_list = [x.getUid() for x in data_ingestion_line_list]
 data_stream_list = context.getPortalObject().portal_catalog(
   portal_type="Data Stream",
   aggregate__related__uid=data_ingestion_uid_list
-)
+)'''
 
 return { "status_code": 0, "result": len(data_stream_list) }
