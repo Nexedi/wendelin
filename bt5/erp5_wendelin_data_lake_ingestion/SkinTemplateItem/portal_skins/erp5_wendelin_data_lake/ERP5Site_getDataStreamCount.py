2d7c54e72b3bfed119127813b4bb47c4c641c781
Roque Porchetto <roque.porchetto@nexedi.com>
2021-02-24 15:33:40 +0000

    erp5_wendelin_data_lake_ingestion: cleanup comments on new scripts

--- bt5/erp5_wendelin_data_lake_ingestion/SkinTemplateItem/portal_skins/erp5_wendelin_data_lake/ERP5Site_getDataStreamCount.py
+++ bt5/erp5_wendelin_data_lake_ingestion/SkinTemplateItem/portal_skins/erp5_wendelin_data_lake/ERP5Site_getDataStreamCount.py
@@ -16,15 +16,4 @@ except Exception as e:
 data_set_uid = data_set.getUid()
 data_stream_list = context.getDataStreamsByDataSet(data_set_uid)
 
-'''catalog_kw = {'portal_type': 'Data Ingestion Line',
-              'aggregate_uid': data_set_uid
-              }
-data_ingestion_line_list = context.portal_catalog(**catalog_kw)
-data_ingestion_uid_list = [x.getUid() for x in data_ingestion_line_list]
-
-data_stream_list = context.getPortalObject().portal_catalog(
-  portal_type="Data Stream",
-  aggregate__related__uid=data_ingestion_uid_list
-)'''
-
 return { "status_code": 0, "result": len(data_stream_list) }
