1125050786f02cf389fa25d768194ede8bb82151
Roque Porchetto <roque.porchetto@nexedi.com>
2021-03-02 13:30:26 +0000

    erp5_wendelin: rename script

--- bt5/erp5_wendelin/SkinTemplateItem/portal_skins/erp5_wendelin/DataSet_getDataStreamList.py
+++ bt5/erp5_wendelin/SkinTemplateItem/portal_skins/erp5_wendelin/DataSet_getDataStreamList.py
@@ -1,3 +1,6 @@
+"""
+This script will return all Data streams for Data set
+"""
 catalog_kw = {'portal_type': 'Data Ingestion Line',
               'aggregate_uid': data_set_uid,
               'limit': limit,
@@ -7,6 +10,7 @@ if data_ingestion_line_list:
   data_ingestion_uid_list = [x.uid for x in data_ingestion_line_list]
   catalog_kw = {'portal_type': 'Data Stream',
                 'aggregate__related__uid': data_ingestion_uid_list,
+                'validation_state':'validated',
                 'select_list': ['reference', 'relative_url', 'versioning.size', 'versioning.version'],
                 }
   return context.getPortalObject().portal_catalog(**catalog_kw)
