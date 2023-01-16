26c63f5bb06b85eed0c2723399ec8ab1c8cfa7b3
Roque Porchetto <roque.porchetto@nexedi.com>
2023-01-16 15:50:31 -0300

    Drone ingestion utils py scripts - run selenium on a headless chrome to use the drone app intensively - ingest generated app logs to wendelin - use frontend remote server

---
+++ utils/drone-app_selenium_headless-chrome.py
@@ -0,0 +1,81 @@
+INPUT_ID_LIST=["simulation_speed", "simulation_time", "drone_speed"]
+INPUT_VALUE_LIST=[300, 1000, 10]
+NUMBER_OF_DRONES = 3
+
+import certifi
+import urllib3
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.remote.remote_connection import RemoteConnection
+
+options = Options()
+options.add_argument('headless')
+options.add_argument('window-size=1200x2600')
+dc = DesiredCapabilities.CHROME
+dc['loggingPrefs'] = { 'browser':'ALL'}
+#local selenium
+#driver = webdriver.Chrome(options=options, desired_capabilities=dc)
+
+#selenium remote server
+server_url = "https://selenium:jvT0SRR9Mtad@softinst179949.host.vifib.net/wd/hub"
+executor = RemoteConnection(server_url, keep_alive=True)
+cert_reqs = 'CERT_REQUIRED'
+ca_certs = certifi.where()
+executor._conn = urllib3.PoolManager(cert_reqs=cert_reqs, ca_certs=ca_certs)
+
+driver = webdriver.Remote(
+    command_executor=executor,
+    desired_capabilities=dc,
+)
+
+# if all ok, use 'browser' instead of local webdriver created on var 'driver'
+
+url = "https://dronesimulator.app.officejs.com/"
+driver.get(url)
+driver.implicitly_wait(5)
+skip = driver.find_element(By.XPATH, '//a[@class="skip-link" and text()="Skip"]')
+skip.click()
+
+#wait for editor iframe content
+driver.implicitly_wait(10)
+iframe = driver.find_element(By.XPATH, '//iframe')
+
+for i, input_id in enumerate(INPUT_ID_LIST):
+  input = driver.find_element(By.ID, input_id)
+  input.clear()
+  input.send_keys(INPUT_VALUE_LIST[i])
+
+input = driver.find_element(By.ID, "number_of_drones")
+input.clear()
+input.send_keys(NUMBER_OF_DRONES)
+
+# how to change iframe html content??
+#frame = driver.find_element(By.XPATH, '//iframe')
+#driver.switch_to.frame(frame)
+#do stuff
+#codemirror = driver.find_elements_by_class_name("CodeMirror-code")
+#driver.switch_to.default_content()
+
+run_button = driver.find_element(By.XPATH, '//input[@type="submit" and @name="action_run"]')
+run_button.click()
+
+loading = driver.find_element(By.XPATH, '//span[@id="loading"]')
+driver.implicitly_wait(20)
+
+for i in range(NUMBER_OF_DRONES):
+  text = "Download Simulation LOG " + str(i)
+  download_log = driver.find_element(By.XPATH, '//div[@class="container"]//a[contains(text(), "' + text + '")]')
+  download_log.click() #saves log txt file in command location
+
+# at this point, we have all the drone logs generated as txt files in command execution directory
+
+#print browser log entries
+for entry in driver.get_log('browser'):
+  print(entry)
+
+#screenshot
+#driver.get_screenshot_as_file('main-page.png')
+
+driver.quit()
