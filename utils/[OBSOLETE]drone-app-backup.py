939cb998bc05a299178016894cc3d85102ef16f2
Roque Porchetto <roque.porchetto@nexedi.com>
2023-01-16 15:50:31 -0300

    Drone ingestion utils py scripts - run selenium on a headless chrome to use the drone app intensively - ingest generated app logs to wendelin - use frontend remote server - script to generate data locally - slapos script to request instances

---
+++ utils/[OBSOLETE]drone-app-backup.py
@@ -0,0 +1,93 @@
+NUMBER_OF_DRONES = 1
+INPUT_ID_LIST=["simulation_speed", "simulation_time", "drone_speed", "number_of_drones"]
+INPUT_VALUE_LIST=[300, 1000, 16, NUMBER_OF_DRONES]
+AI_SCRIPT = 'me.onStart = function () {console.log("start");};me.onUpdate = function (timestamp) {console.log("update!");};'
+
+import certifi
+import urllib3
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.remote.remote_connection import RemoteConnection
+
+# configure the web driver settings
+options = Options()
+options.add_argument('headless')
+options.add_argument('--headless=new')
+options.add_argument('incognito')
+options.add_argument('--no-sandbox')
+options.add_argument('window-size=1200x2600')
+dc = DesiredCapabilities.CHROME
+dc['loggingPrefs'] = { 'browser':'ALL'}
+
+# local selenium
+#driver = webdriver.Chrome(options=options, desired_capabilities=dc)
+
+# run selenium remote server
+server_url = "https://selenium:jvT0SRR9Mtad@softinst179949.host.vifib.net/wd/hub"
+executor = RemoteConnection(server_url, keep_alive=True)
+cert_reqs = 'CERT_REQUIRED'
+ca_certs = certifi.where()
+executor._conn = urllib3.PoolManager(cert_reqs=cert_reqs, ca_certs=ca_certs)
+driver = webdriver.Remote(
+    command_executor=executor,
+    desired_capabilities=dc,
+)
+
+# navigate to drone app
+url = "https://dronesimulator.app.officejs.com/"
+#url = "https://softinst157899.host.vifib.net/erp5/web_site_module/officejs_drone_simulator/"
+driver.get(url)
+driver.implicitly_wait(5)
+# skip bootloader
+skip = driver.find_element(By.XPATH, '//a[@class="skip-link" and text()="Skip"]')
+skip.click()
+
+# wait for editor iframe content
+driver.implicitly_wait(10)
+iframe = driver.find_element(By.XPATH, '//iframe')
+
+# fill all standard inputs
+for i, input_id in enumerate(INPUT_ID_LIST):
+  input = driver.find_element(By.ID, input_id)
+  input.clear()
+  input.send_keys(INPUT_VALUE_LIST[i])
+
+# fill codemirror editor input
+frame = driver.find_element(By.XPATH, '//iframe')
+driver.switch_to.frame(frame)
+#driver.execute_script('return document.getElementsByClassName("CodeMirror")[0].CodeMirror.setValue("' + AI_SCRIPT + '")')
+driver.switch_to.default_content()
+
+#run the simulation
+run_button = driver.find_element(By.XPATH, '//input[@type="submit" and @name="action_run"]')
+run_button.click()
+
+driver.implicitly_wait(30)
+loading = driver.find_element(By.XPATH, '//span[@id="loading"]')
+
+time.sleep(20)
+#screenshot
+driver.get_screenshot_as_file('main-page.png')
+
+# download all result logs
+for i in range(NUMBER_OF_DRONES):
+  text = "Download Simulation LOG " + str(i)
+  id_s = "log_result_" + str(i)
+  download_log = driver.find_element(By.XPATH, '//div[@class="container"]//a[contains(text(), "' + text + '")]')
+  #download_log.click() #saves log txt file in command location
+  result_log = driver.find_element(By.XPATH, '//div[@class="container"]//textarea[@id="' + id_s + '"]')
+  result_list[i] = result_log.get_attribute('value')
+
+# at this point, we have all the drone logs generated as txt files in command execution directory
+
+#screenshot
+driver.get_screenshot_as_file('main-page.png')
+
+#print browser log entries
+for entry in driver.get_log('browser'):
+  print(entry)
+
+driver.quit()
---
+++ utils/script-slapos-request.py
@@ -0,0 +1,64 @@
+import time
+#from slapos import request #missing eggs! "No module named slapos"
+
+NUMBER_OF_SERVERS = 2
+
+selenium_SR_url = "~/srv/project/slapos/software/seleniumserver/software.cfg"
+selenium_SR_url = "~/srv/project/slapos/software/seleniumrunner/software.cfg"
+#TODO how to get SR url?
+selenium_SR_url = "/srv/slapgrid/slappart6/srv/project/slapos/software/seleniumserver/software.cfg"
+
+instance_list = []
+
+def request_instances(NUMBER_OF_SERVERS):
+  for i in range(NUMBER_OF_SERVERS):
+    instance = request(selenium_SR_url, "seleniumserver-" + str(i))
+    instance_list.append(instance)
+
+done = False
+nap = 300
+while not done:
+  request_instances(NUMBER_OF_SERVERS)
+  done = True
+  for instance in instance_list:
+    print("checking instance in %s" % instance.getId())
+    print("instance started?")
+    if instance.getState() != 'started':
+      print("instance NOT started")
+      time.sleep(nap)
+      done = False
+      continue
+    print("instance started!")
+    print("instance ready?")
+    try:
+      instance.getConnectionParameter('url')
+    except:
+      print("instance NOT ready")
+      time.sleep(nap)
+      done = False
+      continue
+    print("instance in %s ready!" % instance.getId())
+
+print("all instances ready")
+
+server_url_list = []
+
+print("Server urls:")
+
+for instance in instance_list:
+  # get a specific parameter info
+  server_url = instance.getConnectionParameter('url')
+  server_url_list.append(server_url)
+  print(server_url)
+  # e.g. 'https://selenium:jvT0SRR9Mtad@[2001:67c:1254:5f:d58a::5933]:9443/wd/hub'
+  #instance.getConnectionParameter('connection-parameters')
+  # ?? ERROR, not found
+
+#OUTSIDE (slapos console)
+#slapos service list
+#{
+#  "seleniumserver": "~/srv/project/slapos/software/seleniumserver/software.cfg"
+#}
+
+#slapos service info seleniumserver
+# dict with all info
