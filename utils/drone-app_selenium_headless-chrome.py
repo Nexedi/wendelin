a5f1b91f20fcd93fe3327fe2f069fa28d9445f93
Roque Porchetto <roque.porchetto@nexedi.com>
2023-02-08 16:03:29 +0100

    update selenium script

--- utils/drone-app_selenium_headless-chrome.py
+++ utils/drone-app_selenium_headless-chrome.py
@@ -1,6 +1,5 @@
-INPUT_ID_LIST=["simulation_speed", "simulation_time", "drone_speed"]
-INPUT_VALUE_LIST=[300, 1000, 10]
-NUMBER_OF_DRONES = 3
+INPUT_ID_LIST=["simulation_speed", "simulation_time", "drone_speed", "number_of_drones"]
+INPUT_VALUE_LIST=[300, 1000, 10, 3]
 
 import certifi
 import urllib3
@@ -10,15 +9,17 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.remote.remote_connection import RemoteConnection
 
+# configure the web driver settings
 options = Options()
 options.add_argument('headless')
 options.add_argument('window-size=1200x2600')
 dc = DesiredCapabilities.CHROME
 dc['loggingPrefs'] = { 'browser':'ALL'}
-#local selenium
+
+# local selenium
 #driver = webdriver.Chrome(options=options, desired_capabilities=dc)
 
-#selenium remote server
+# run selenium remote server
 server_url = "https://selenium:jvT0SRR9Mtad@softinst179949.host.vifib.net/wd/hub"
 executor = RemoteConnection(server_url, keep_alive=True)
 cert_reqs = 'CERT_REQUIRED'
@@ -30,40 +31,39 @@ driver = webdriver.Remote(
     desired_capabilities=dc,
 )
 
-# if all ok, use 'browser' instead of local webdriver created on var 'driver'
-
+# navigate to drone app
 url = "https://dronesimulator.app.officejs.com/"
 driver.get(url)
 driver.implicitly_wait(5)
+# skip bootloader
 skip = driver.find_element(By.XPATH, '//a[@class="skip-link" and text()="Skip"]')
 skip.click()
 
-#wait for editor iframe content
+# wait for editor iframe content
 driver.implicitly_wait(10)
 iframe = driver.find_element(By.XPATH, '//iframe')
 
+# fill all standard inputs
 for i, input_id in enumerate(INPUT_ID_LIST):
   input = driver.find_element(By.ID, input_id)
   input.clear()
   input.send_keys(INPUT_VALUE_LIST[i])
 
-input = driver.find_element(By.ID, "number_of_drones")
-input.clear()
-input.send_keys(NUMBER_OF_DRONES)
+# fill codemirror editor input
+script = "code..."
+frame = driver.find_element(By.XPATH, '//iframe')
+driver.switch_to.frame(frame)
+driver.execute_script('return document.getElementsByClassName("CodeMirror")[0].CodeMirror.setValue("my amazing AI script")')
+driver.switch_to.default_content()
 
-# how to change iframe html content??
-#frame = driver.find_element(By.XPATH, '//iframe')
-#driver.switch_to.frame(frame)
-#do stuff
-#codemirror = driver.find_elements_by_class_name("CodeMirror-code")
-#driver.switch_to.default_content()
-
+#run the simulation
 run_button = driver.find_element(By.XPATH, '//input[@type="submit" and @name="action_run"]')
 run_button.click()
 
 loading = driver.find_element(By.XPATH, '//span[@id="loading"]')
 driver.implicitly_wait(20)
 
+# download all result logs
 for i in range(NUMBER_OF_DRONES):
   text = "Download Simulation LOG " + str(i)
   download_log = driver.find_element(By.XPATH, '//div[@class="container"]//a[contains(text(), "' + text + '")]')
