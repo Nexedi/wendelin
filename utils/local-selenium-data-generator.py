77c4247a298e2577d1d277b37e5a4bcd141da816
Roque Porchetto <roque.porchetto@nexedi.com>
2023-02-13 20:16:13 +0100

    update selenium script

--- utils/local-selenium-data-generator.py
+++ utils/local-selenium-data-generator.py
@@ -1,15 +1,38 @@
-NUMBER_OF_DRONES = 1
-INPUT_ID_LIST=["simulation_speed", "simulation_time", "drone_speed", "number_of_drones"]
-INPUT_VALUE_LIST=[300, 1000, 16, NUMBER_OF_DRONES]
-
+import time
+import itertools
 import certifi
 import urllib3
-import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.remote.remote_connection import RemoteConnection
+from datetime import datetime
+
+print("Start execution at:")
+print(datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+
+NUMBER_OF_DRONES = 1
+GRANULARITY = 3
+GAME_INPUT_ID_LIST=["simulation_speed", "simulation_time", "number_of_drones", "drone_min_speed", "drone_speed", "drone_max_speed", "drone_max_acceleration", "drone_max_deceleration"]
+GAME_INPUT_VALUE_LIST=[300, 1500, NUMBER_OF_DRONES, 10, 15, 30, 6, 1]
+DRONE_INPUT_ID_LIST=["drone_max_roll", "drone_min_pitch", "drone_max_pitch", "drone_max_sink_rate", "drone_max_climb_rate"]
+DRONE_VALUE_RANGE_LIST=[[0, 90], [-50, 0], [0, 50], [-15, 30], [-1, 30]]
+DRONE_INPUT_VALUE_LIST=[]
+
+def values_in_range(start, end, n):
+  d = (end - start) / (n - 1)
+  return [start + i*d for i in range(n)]
+
+for i in range(len(DRONE_VALUE_RANGE_LIST)):
+  DRONE_INPUT_VALUE_LIST.append(values_in_range(DRONE_VALUE_RANGE_LIST[i][0], DRONE_VALUE_RANGE_LIST[i][1], GRANULARITY))
+
+#speed  1 - 30 (min<sp<max)
+#accel  1 - 10
+#roll   0 - 90
+#pitch -50 - 50 (min<max)
+#sink  -15 - 30  (<max_speed)
+#climb  -1 - 30  (<max_speed)
 
 # configure the web driver settings
 options = Options()
@@ -24,6 +47,7 @@ driver = webdriver.Chrome(options=options, desired_capabilities=dc)
 
 # navigate to drone app
 url = "https://dronesimulator.app.officejs.com/"
+url = "https://softinst157899.host.vifib.net/erp5/web_site_module/officejs_drone_simulator/"
 driver.get(url)
 driver.implicitly_wait(5)
 # skip bootloader
@@ -34,33 +58,37 @@ skip.click()
 driver.implicitly_wait(10)
 iframe = driver.find_element(By.XPATH, '//iframe')
 
-# fill all standard inputs
-for i, input_id in enumerate(INPUT_ID_LIST):
+# fill game inputs
+for i, input_id in enumerate(GAME_INPUT_ID_LIST):
   input = driver.find_element(By.ID, input_id)
   input.clear()
-  input.send_keys(INPUT_VALUE_LIST[i])
+  input.send_keys(GAME_INPUT_VALUE_LIST[i])
 
-#run the simulation
-run_button = driver.find_element(By.XPATH, '//input[@type="submit" and @name="action_run"]')
-run_button.click()
+# run all combination of input values
+for combination in itertools.product(*DRONE_INPUT_VALUE_LIST):
+  # fill drone inputs
+  print("* running combination " + str(combination))
+  for i, input_id in enumerate(DRONE_INPUT_ID_LIST):
+    input = driver.find_element(By.ID, input_id)
+    input.clear()
+    input.send_keys(int(combination[i]))
 
-driver.implicitly_wait(30)
-loading = driver.find_element(By.XPATH, '//span[@id="loading"]')
-time.sleep(20)
+  #run the simulation
+  run_button = driver.find_element(By.XPATH, '//input[@type="submit" and @name="action_run"]')
+  run_button.click()
 
-# download all result logs
-for i in range(NUMBER_OF_DRONES):
-  text = "Download Simulation LOG " + str(i)
-  download_log = driver.find_element(By.XPATH, '//div[@class="container"]//a[contains(text(), "' + text + '")]')
-  download_log.click() #saves log txt file in command location
+  driver.implicitly_wait(30)
+  loading = driver.find_element(By.XPATH, '//span[@id="loading"]')
+  time.sleep(20)
 
-# at this point, we have all the drone logs generated as txt files in command execution directory
+  # download all result logs
+  for i in range(NUMBER_OF_DRONES):
+    text = "Download Simulation LOG " + str(i)
+    download_log = driver.find_element(By.XPATH, '//div[@class="container"]//a[contains(text(), "' + text + '")]')
+    download_log.click() #saves log txt file in command location
 
+print("End execution at:")
+print(datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
 #screenshot
-driver.get_screenshot_as_file('main-page.png')
+#driver.get_screenshot_as_file('main-page.png')
 
-#print browser log entries
-for entry in driver.get_log('browser'):
-  print(entry)
-
-driver.quit()
