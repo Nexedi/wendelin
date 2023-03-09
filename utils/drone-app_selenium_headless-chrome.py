41ac82e64edb5e312d44c46bd02fcbf4c2dd81a9
Roque Porchetto <roque.porchetto@nexedi.com>
2023-03-09 19:02:28 +0100

    unify scripts

--- utils/drone-app_selenium_headless-chrome.py
+++ utils/drone-app_selenium_headless-chrome.py
@@ -1,93 +1,171 @@
-NUMBER_OF_DRONES = 1
-INPUT_ID_LIST=["simulation_speed", "simulation_time", "drone_speed", "number_of_drones"]
-INPUT_VALUE_LIST=[300, 1000, 16, NUMBER_OF_DRONES]
-AI_SCRIPT = 'me.onStart = function () {console.log("start");};me.onUpdate = function (timestamp) {console.log("update!");};'
-
+import sys
+import time
+import itertools
 import certifi
 import urllib3
-import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.remote.remote_connection import RemoteConnection
+from datetime import datetime
+
+start_time = datetime.now()
+print("Start execution at:")
+print(start_time.strftime("%d/%m/%Y %H:%M:%S"))
+
+NUMBER_OF_DRONES = 1
+GRANULARITY = 8
+GAME_INPUT_ID_LIST=["simulation_speed", "simulation_time", "number_of_drones", "drone_min_speed", "drone_max_speed", "drone_max_acceleration", "drone_max_deceleration", "start_AMSL", "init_pos_lat", "init_pos_lon", "init_pos_z", "drone_max_sink_rate", "drone_max_climb_rate"]
+GAME_INPUT_VALUE_LIST=[1500*60, 1500, NUMBER_OF_DRONES, 12, 20, 6, 1, "594.792", "45.6403", "14.2648", "65.668", 3, 8]
+DRONE_INPUT_ID_LIST=["drone_speed", "drone_max_roll", "drone_min_pitch", "drone_max_pitch"]
+DRONE_VALUE_RANGE_LIST=[[12.1, 19.9], [10, 50], [-35, -10], [10, 40]]
+#DRONE_VALUE_RANGE_LIST=[[12, 52], [-37, -11], [12, 38]]
+DRONE_INPUT_VALUE_LIST=[]
+AI_SCRIPT = ""
+if (len(sys.argv)>1):
+  AI_SCRIPT = open(sys.argv[1], "r").read().replace("\n", "\\n").replace('"', "'")
+# flag to set if run remote or local
+REMOTE = False
+
+def values_in_range(start, end, n):
+  d = (end - start) / (n - 1)
+  return [start + i*d for i in range(n)]
+
+for i in range(len(DRONE_VALUE_RANGE_LIST)):
+  DRONE_INPUT_VALUE_LIST.append(values_in_range(DRONE_VALUE_RANGE_LIST[i][0], DRONE_VALUE_RANGE_LIST[i][1], GRANULARITY))
+
+def setup_driver_on_app(options, dc):
+  done = False
+  retry = 0
+  while not done:
+    if retry == 10:
+        raise RuntimeError("Error: couldn't create webdriver after 10 attempts")
+    try:
+        if (REMOTE):
+          # run selenium remote server
+          server_url = "https://selenium:jvT0SRR9Mtad@softinst179949.host.vifib.net/wd/hub"
+          executor = RemoteConnection(server_url, keep_alive=True)
+          cert_reqs = 'CERT_REQUIRED'
+          ca_certs = certifi.where()
+          executor._conn = urllib3.PoolManager(cert_reqs=cert_reqs, ca_certs=ca_certs)
+          driver = webdriver.Remote(
+              command_executor=executor,
+              desired_capabilities=dc,
+          )
+        else:
+          # local selenium
+          driver = webdriver.Chrome(options=options, desired_capabilities=dc)
+
+        # navigate to drone app
+        url = "https://dronesimulator.app.officejs.com/"
+        #url = "https://softinst157899.host.vifib.net/erp5/web_site_module/officejs_drone_simulator/"
+        driver.get(url)
+        driver.implicitly_wait(5)
+        # skip bootloader
+        try:
+          skip = driver.find_element(By.XPATH, '//a[@class="skip-link" and text()="Skip"]')
+          skip.click()
+        except:
+          # if app was installed  on a previous run, there is no bootloader page. contine
+          pass
+
+        # wait for editor iframe content
+        driver.implicitly_wait(20)
+        iframe = driver.find_element(By.XPATH, '//iframe')
+
+        # fill game inputs
+        for i, input_id in enumerate(GAME_INPUT_ID_LIST):
+          input = driver.find_element(By.ID, input_id)
+          input.clear()
+          input.send_keys(GAME_INPUT_VALUE_LIST[i])
+
+        # fill codemirror editor input
+        if (AI_SCRIPT):
+            frame = driver.find_element(By.XPATH, '//iframe')
+            driver.switch_to.frame(frame)
+            driver.execute_script('return document.getElementsByClassName("CodeMirror")[0].CodeMirror.setValue("' + AI_SCRIPT + '")')
+            driver.switch_to.default_content()
+            #driver.get_screenshot_as_file('ai.png')
+
+        print("Webdriver created.")
+        done = True
+    except Exception as e:
+        print("Error creating webdriver.")
+        print(e)
+        print("retry")
+        retry += 1
+  return driver
 
 # configure the web driver settings
 options = Options()
 options.add_argument('headless')
 options.add_argument('--headless=new')
 options.add_argument('incognito')
+options.add_argument('window-size=1200x1200')
 options.add_argument('--no-sandbox')
-options.add_argument('window-size=1200x2600')
+options.add_argument('--disable-dev-shm-usage')        
 dc = DesiredCapabilities.CHROME
 dc['loggingPrefs'] = { 'browser':'ALL'}
 
-# local selenium
-#driver = webdriver.Chrome(options=options, desired_capabilities=dc)
+driver = setup_driver_on_app(options, dc)
+iter = 0
+# run all combination of input values
+for combination in itertools.product(*DRONE_INPUT_VALUE_LIST):
+  iter += 1
+  try:
+    # fill drone inputs
+    print("Running combination " + str(combination))
+    #print(datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+    start_iter = datetime.now()
+    for i, input_id in enumerate(DRONE_INPUT_ID_LIST):
+      input = driver.find_element(By.ID, input_id)
+      input.clear()
+      input.send_keys(str(combination[i]))
+      #driver.get_screenshot_as_file(str(combination) + '.png')
 
-# run selenium remote server
-server_url = "https://selenium:jvT0SRR9Mtad@softinst179949.host.vifib.net/wd/hub"
-executor = RemoteConnection(server_url, keep_alive=True)
-cert_reqs = 'CERT_REQUIRED'
-ca_certs = certifi.where()
-executor._conn = urllib3.PoolManager(cert_reqs=cert_reqs, ca_certs=ca_certs)
-driver = webdriver.Remote(
-    command_executor=executor,
-    desired_capabilities=dc,
-)
+    #'''
+    #run the simulation
+    run_button = driver.find_element(By.XPATH, '//input[@type="submit" and @name="action_run"]')
+    run_button.click()
 
-# navigate to drone app
-url = "https://dronesimulator.app.officejs.com/"
-#url = "https://softinst157899.host.vifib.net/erp5/web_site_module/officejs_drone_simulator/"
-driver.get(url)
-driver.implicitly_wait(5)
-# skip bootloader
-skip = driver.find_element(By.XPATH, '//a[@class="skip-link" and text()="Skip"]')
-skip.click()
+    driver.implicitly_wait(30)
+    loading = driver.find_element(By.XPATH, '//span[@id="loading"]')
+    driver.find_element(By.XPATH, '//div[@class="container"]//a[contains(text(), "Download Simulation LOG")]')
 
-# wait for editor iframe content
-driver.implicitly_wait(10)
-iframe = driver.find_element(By.XPATH, '//iframe')
+    # download/store all result logs
+    result_list = []
+    for i in range(NUMBER_OF_DRONES):
+      text = "Download Simulation LOG " + str(i)
+      download_log = driver.find_element(By.XPATH, '//div[@class="container"]//a[contains(text(), "' + text + '")]')
+      download_log.click() #saves log txt file in command location
+      # stores log result on object (can be used to send to wendelin)
+      result_log = driver.find_element(By.XPATH, '//div[@class="container"]//textarea[@id="' + id_s + '"]')
+      result_list.append(result_log.get_attribute('value'))
 
-# fill all standard inputs
-for i, input_id in enumerate(INPUT_ID_LIST):
-  input = driver.find_element(By.ID, input_id)
-  input.clear()
-  input.send_keys(INPUT_VALUE_LIST[i])
+    end_iter = datetime.now()
+    elapsed = end_iter - start_iter
+    print("* done in %s milliseconds: " % str(elapsed.total_seconds()*1000))
+    #'''
+  except Exception as e:
+    print("ERROR: simulation for this combination failed")
+    print(e)
+    print("trying to take screenshot...")
+    try:
+      driver.get_screenshot_as_file(str(combination) + '.png')
+    except:
+      print("Error while taking screenshot, this means driver sesion ended. Re-creating webdriver...")
+      driver = setup_driver_on_app(options, dc)
 
-# fill codemirror editor input
-frame = driver.find_element(By.XPATH, '//iframe')
-driver.switch_to.frame(frame)
-#driver.execute_script('return document.getElementsByClassName("CodeMirror")[0].CodeMirror.setValue("' + AI_SCRIPT + '")')
-driver.switch_to.default_content()
-
-#run the simulation
-run_button = driver.find_element(By.XPATH, '//input[@type="submit" and @name="action_run"]')
-run_button.click()
-
-driver.implicitly_wait(30)
-loading = driver.find_element(By.XPATH, '//span[@id="loading"]')
-
-time.sleep(20)
-#screenshot
-driver.get_screenshot_as_file('main-page.png')
-
-# download all result logs
-for i in range(NUMBER_OF_DRONES):
-  text = "Download Simulation LOG " + str(i)
-  id_s = "log_result_" + str(i)
-  download_log = driver.find_element(By.XPATH, '//div[@class="container"]//a[contains(text(), "' + text + '")]')
-  #download_log.click() #saves log txt file in command location
-  result_log = driver.find_element(By.XPATH, '//div[@class="container"]//textarea[@id="' + id_s + '"]')
-  result_list[i] = result_log.get_attribute('value')
-
-# at this point, we have all the drone logs generated as txt files in command execution directory
-
-#screenshot
-driver.get_screenshot_as_file('main-page.png')
+end_time = datetime.now()
+print("End execution at:")
+print(end_time.now().strftime("%d/%m/%Y %H:%M:%S"))
+elapsed_time = end_time - start_time
+print("Total combinations: " + str(iter))
+print("Total time %s seconds. " % str(elapsed_time.seconds))
 
 #print browser log entries
-for entry in driver.get_log('browser'):
-  print(entry)
+#for entry in driver.get_log('browser'):
+#  print(entry)
 
 driver.quit()
