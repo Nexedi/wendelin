756bb65191afbe1c38981fbdfbdf4b6c73887d8e
Roque Porchetto <roque.porchetto@nexedi.com>
2023-03-20 19:31:15 +0100

    [WIP] running several browsers on paralel

--- utils/drone-app_selenium_headless-chrome.py
+++ utils/drone-app_selenium_headless-chrome.py
@@ -10,23 +10,7 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.remote.remote_connection import RemoteConnection
 from datetime import datetime
 
-start_time = datetime.now()
-print("Start execution at:")
-print(start_time.strftime("%d/%m/%Y %H:%M:%S"))
-
-NUMBER_OF_DRONES = 1
-GRANULARITY = 8
-GAME_INPUT_ID_LIST=["simulation_speed", "simulation_time", "number_of_drones", "drone_min_speed", "drone_max_speed", "drone_max_acceleration", "drone_max_deceleration", "start_AMSL", "init_pos_lat", "init_pos_lon", "init_pos_z", "drone_max_sink_rate", "drone_max_climb_rate"]
-GAME_INPUT_VALUE_LIST=[1500*60, 1500, NUMBER_OF_DRONES, 12, 20, 6, 1, "594.792", "45.6403", "14.2648", "65.668", 3, 8]
-DRONE_INPUT_ID_LIST=["drone_speed", "drone_max_roll", "drone_min_pitch", "drone_max_pitch"]
-DRONE_VALUE_RANGE_LIST=[[12.1, 19.9], [10, 50], [-35, -10], [10, 40]]
-#DRONE_VALUE_RANGE_LIST=[[12, 52], [-37, -11], [12, 38]]
-DRONE_INPUT_VALUE_LIST=[]
-AI_SCRIPT = ""
-if (len(sys.argv)>1):
-  AI_SCRIPT = open(sys.argv[1], "r").read().replace("\n", "\\n").replace('"', "'")
-# flag to set if run remote or local
-REMOTE = False
+##### FUNCTIONS DEFINITION #####
 
 def values_in_range(start, end, n):
   if n == 1:
@@ -34,70 +18,98 @@ def values_in_range(start, end, n):
   d = (end - start) / (n - 1)
   return [start + i*d for i in range(n)]
 
-for i in range(len(DRONE_VALUE_RANGE_LIST)):
-  DRONE_INPUT_VALUE_LIST.append(values_in_range(DRONE_VALUE_RANGE_LIST[i][0], DRONE_VALUE_RANGE_LIST[i][1], GRANULARITY))
-
-def setup_driver_on_app(options, dc):
+def setup_driver_on_app(server_url, options, dc):
+  print("Creating webdriver for " + server_url)
   done = False
   retry = 0
   while not done:
     if retry == 10:
-        raise RuntimeError("Error: couldn't create webdriver after 10 attempts")
+        raise RuntimeError("Error: couldn't create webdriver after 10 attempts for " + server_url)
     try:
-        if (REMOTE):
-          # run selenium remote server
-          server_url = "https://selenium:jvT0SRR9Mtad@softinst179949.host.vifib.net/wd/hub"
-          executor = RemoteConnection(server_url, keep_alive=True)
-          cert_reqs = 'CERT_REQUIRED'
-          ca_certs = certifi.where()
-          executor._conn = urllib3.PoolManager(cert_reqs=cert_reqs, ca_certs=ca_certs)
-          driver = webdriver.Remote(
-              command_executor=executor,
-              desired_capabilities=dc,
-          )
-        else:
-          # local selenium
-          driver = webdriver.Chrome(options=options, desired_capabilities=dc)
+      if (REMOTE):
+        # run selenium remote server
+        executor = RemoteConnection(server_url, keep_alive=True)
+        cert_reqs = 'CERT_REQUIRED'
+        ca_certs = certifi.where()
+        executor._conn = urllib3.PoolManager(cert_reqs=cert_reqs, ca_certs=ca_certs)
+        driver = webdriver.Remote(
+            command_executor=executor,
+            desired_capabilities=dc,
+        )
+      else:
+        # local selenium
+        driver = webdriver.Chrome(options=options, desired_capabilities=dc)
 
-        # navigate to drone app
-        url = "https://dronesimulator.app.officejs.com/"
-        #url = "https://softinst157899.host.vifib.net/erp5/web_site_module/officejs_drone_simulator/"
-        driver.get(url)
-        driver.implicitly_wait(5)
-        # skip bootloader
-        try:
-          skip = driver.find_element(By.XPATH, '//a[@class="skip-link" and text()="Skip"]')
-          skip.click()
-        except:
-          # if app was installed  on a previous run, there is no bootloader page. contine
-          pass
+      # navigate to drone app
+      driver.get(APP_URL)
+      driver.implicitly_wait(5)
+      # skip bootloader
+      try:
+        skip = driver.find_element(By.XPATH, '//a[@class="skip-link" and text()="Skip"]')
+        skip.click()
+      except:
+        # if app was installed  on a previous run, there is no bootloader page. contine
+        pass
 
-        # wait for editor iframe content
-        driver.implicitly_wait(20)
-        iframe = driver.find_element(By.XPATH, '//iframe')
+      # wait for editor iframe content
+      driver.implicitly_wait(20)
+      iframe = driver.find_element(By.XPATH, '//iframe')
 
-        # fill game inputs
-        for i, input_id in enumerate(GAME_INPUT_ID_LIST):
-          input = driver.find_element(By.ID, input_id)
-          input.clear()
-          input.send_keys(GAME_INPUT_VALUE_LIST[i])
+      # fill game inputs
+      for i, input_id in enumerate(GAME_INPUT_ID_LIST):
+        input = driver.find_element(By.ID, input_id)
+        input.clear()
+        input.send_keys(GAME_INPUT_VALUE_LIST[i])
 
-        # fill codemirror editor input
-        if (AI_SCRIPT):
-            frame = driver.find_element(By.XPATH, '//iframe')
-            driver.switch_to.frame(frame)
-            driver.execute_script('return document.getElementsByClassName("CodeMirror")[0].CodeMirror.setValue("' + AI_SCRIPT + '")')
-            driver.switch_to.default_content()
-            #driver.get_screenshot_as_file('ai.png')
+      # fill codemirror editor input
+      if (AI_SCRIPT):
+        frame = driver.find_element(By.XPATH, '//iframe')
+        driver.switch_to.frame(frame)
+        driver.execute_script('return document.getElementsByClassName("CodeMirror")[0].CodeMirror.setValue("' + AI_SCRIPT + '")')
+        driver.switch_to.default_content()
+        #driver.get_screenshot_as_file('ai.png')
 
-        print("Webdriver created.")
-        done = True
+      print("Webdriver created for " + server_url)
+      done = True
     except Exception as e:
-        print("Error creating webdriver.")
-        print(e)
-        print("retry")
-        retry += 1
+      print("Error creating webdriver for " + server_url)
+      print(e)
+      print("retry")
+      retry += 1
   return driver
+
+##### MAIN #####
+
+NUMBER_OF_DRONES = 1
+GRANULARITY = 2
+GAME_INPUT_ID_LIST=["simulation_speed", "simulation_time", "number_of_drones", "drone_min_speed", "drone_max_speed", "drone_max_acceleration", "drone_max_deceleration", "start_AMSL", "init_pos_lat", "init_pos_lon", "init_pos_z", "drone_max_sink_rate", "drone_max_climb_rate"]
+GAME_INPUT_VALUE_LIST=[1500*60, 1500, NUMBER_OF_DRONES, 12, 20, 6, 1, "594.792", "45.6403", "14.2648", "65.668", 3, 8]
+#DRONE_INPUT_ID_LIST=["drone_speed", "drone_max_roll", "drone_min_pitch", "drone_max_pitch"]
+#DRONE_VALUE_RANGE_LIST=[[12, 20], [10, 50], [-35, -10], [10, 40]]
+DRONE_INPUT_ID_LIST=["drone_speed"]
+DRONE_VALUE_RANGE_LIST=[[12, 20]]
+DRONE_INPUT_VALUE_LIST=[]
+AI_SCRIPT = ""
+if (len(sys.argv)>1):
+  AI_SCRIPT = open(sys.argv[1], "r").read().replace("\n", "\\n").replace('"', "'")
+REMOTE = True # flag to set if run remote or local
+APP_URL = "https://dronesimulator.app.officejs.com/"
+# TODO. HARDCODED, it shouls be generated by requesting the instances //see: script-slapos-request.py
+server_url_list = [
+  "https://selenium:i1Kzu0Yd8L2R@softinst182376.host.vifib.net/wd/hub",
+  "https://selenium:dBlhn3DRcpgu@softinst182375.host.vifib.net/wd/hub"
+]
+
+for i in range(len(DRONE_VALUE_RANGE_LIST)):
+  DRONE_INPUT_VALUE_LIST.append(values_in_range(DRONE_VALUE_RANGE_LIST[i][0], DRONE_VALUE_RANGE_LIST[i][1], GRANULARITY))
+
+combination_list = itertools.product(*DRONE_INPUT_VALUE_LIST)
+print("full combination_list:")
+print(combination_list)
+
+start_time = datetime.now()
+print("Start execution at:")
+print(start_time.strftime("%d/%m/%Y %H:%M:%S"))
 
 # configure the web driver settings
 options = Options()
@@ -110,7 +122,104 @@ options.add_argument('--disable-dev-shm-usage')
 dc = DesiredCapabilities.CHROME
 dc['loggingPrefs'] = { 'browser':'ALL'}
 
-driver = setup_driver_on_app(options, dc)
+def checkDriver(driver_dict):
+  print("checking driver: " str(driver_dict))
+  driver.implicitly_wait(1)
+  try:
+    driver.find_element(By.XPATH, '//div[@class="container"]//a[contains(text(), "Download Simulation LOG")]')
+    print("driver finished previous comb. Set it to free")
+    #TODO save results
+    #free driver
+    driver_dict.running_combination = None
+  except Exception as e:
+    print("driver is still busy")
+    print("exception on check drive:")
+    print(e)
+
+def getFreeDriver(combination):
+  free_driver = None
+  for driver_dict in driver_list:
+    checkDriver(driver_dict)
+    if not free_driver:
+      if not driver_dict.running_combination:
+        driver_dict.running_combination = combination
+        free_driver = driver_dict
+  return free_driver
+
+def runDriver(driver, combination):
+  # fill drone inputs
+  print("Running combination " + str(combination))
+  for i, input_id in enumerate(DRONE_INPUT_ID_LIST):
+    input = driver.find_element(By.ID, input_id)
+    input.clear()
+    input.send_keys(str(combination[i]))
+  #run the simulation
+  run_button = driver.find_element(By.XPATH, '//input[@type="submit" and @name="action_run"]')
+  run_button.click()
+
+print("setting ALL drivers")
+driver_list = []
+for server_url in server_url_list:
+  driver = setup_driver_on_app(server_url, options, dc)
+  driver_dict = {
+    'server_url': server_url,
+    'driver': driver,
+    'running_combination': None
+  }
+  driver_list.append(driver_dict)
+print("Done. Driver dict list:")
+print(driver_list)
+
+while len(combination_list) > 0:
+  print("---------------------------------")
+  print("len(combination_list): " + str(len(combination_list)))
+  print("Allocating combination " + str(combination))
+  combination = combination_list.pop()
+  assigned = False
+  while not assigned:
+    print("checking drivers...")
+    # check drivers
+    driver_dict = getFreeDriver(combination)
+    print("get free driver result:")
+    print(driver_dict)
+    if driver_dict:
+      assigned = True
+      print("combination assigned to free driver. running driver combination")
+      try:
+        runDriver(driver_dict.driver, combination)
+      except Exception as e:
+        print("ERROR: simulation for this combination failed")
+        print(e)
+        assigned = False
+        print("trying to take screenshot...")
+        try:
+          driver_dict.driver.get_screenshot_as_file(str(combination) + '.png')
+        except:
+          print("Error while taking screenshot, this means driver sesion died. Re-creating webdriver...")
+          driver_dict.driver = setup_driver_on_app(driver_dict.server_url, options, dc)
+          driver_dict.running_combination = None
+    else:
+      print("no free drivers yet")
+      time.sleep(1) #wait
+
+print("len(combination_list): " + str(len(combination_list)))
+
+end_time = datetime.now()
+print("End execution at:")
+print(end_time.now().strftime("%d/%m/%Y %H:%M:%S"))
+elapsed_time = end_time - start_time
+print("Total combinations: " + str(iter))
+print("Total time %s seconds. " % str(elapsed_time.seconds))
+
+#print browser log entries
+#for entry in driver.get_log('browser'):
+#  print(entry)
+
+driver.quit()
+
+
+# SAVE OLD CODE:
+'''
 iter = 0
 # run all combination of input values
 for combination in itertools.product(*DRONE_INPUT_VALUE_LIST):
@@ -126,7 +235,6 @@ for combination in itertools.product(*DRONE_INPUT_VALUE_LIST):
       input.send_keys(str(combination[i]))
       #driver.get_screenshot_as_file(str(combination) + '.png')
 
-    #'''
     #run the simulation
     run_button = driver.find_element(By.XPATH, '//input[@type="submit" and @name="action_run"]')
     run_button.click()
@@ -149,7 +257,6 @@ for combination in itertools.product(*DRONE_INPUT_VALUE_LIST):
     end_iter = datetime.now()
     elapsed = end_iter - start_iter
     print("* done in %s milliseconds: " % str(elapsed.total_seconds()*1000))
-    #'''
   except Exception as e:
     print("ERROR: simulation for this combination failed")
     print(e)
@@ -158,17 +265,5 @@ for combination in itertools.product(*DRONE_INPUT_VALUE_LIST):
       driver.get_screenshot_as_file(str(combination) + '.png')
     except:
       print("Error while taking screenshot, this means driver sesion ended. Re-creating webdriver...")
-      driver = setup_driver_on_app(options, dc)
-
-end_time = datetime.now()
-print("End execution at:")
-print(end_time.now().strftime("%d/%m/%Y %H:%M:%S"))
-elapsed_time = end_time - start_time
-print("Total combinations: " + str(iter))
-print("Total time %s seconds. " % str(elapsed_time.seconds))
-
-#print browser log entries
-#for entry in driver.get_log('browser'):
-#  print(entry)
-
-driver.quit()
+      driver = setup_driver_on_app(options, dc)+
+'''
