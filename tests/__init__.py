7c4bc11c86bf03e5f2afa6d33029299bcdcb2afd
mmanchev23 <mmanchev2003@gmail.com>
2023-03-21 20:25:35 +0200

    Initial commit ...

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,6 +4,7 @@ from test_suite import ERP5TypeTestSuite
 import glob
 import os.path
 import re
+from itertools import chain
 
 HERE = os.path.dirname(__file__)
 BT5 = os.path.join(os.path.split(HERE)[0], 'bt5')
