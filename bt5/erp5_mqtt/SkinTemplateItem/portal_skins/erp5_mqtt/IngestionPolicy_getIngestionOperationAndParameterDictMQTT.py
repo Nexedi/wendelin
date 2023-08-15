47fd592f97e39b5c94d893297badc460608f6dde
Martin Manchev <mmanchev23@gmail.com>
2023-08-15 13:01:34 +0200

    Added Data Operation, Supply and Scripts ...

--- bt5/erp5_mqtt/SkinTemplateItem/portal_skins/erp5_mqtt/IngestionPolicy_getIngestionOperationAndParameterDictMQTT.py
+++ bt5/erp5_mqtt/SkinTemplateItem/portal_skins/erp5_mqtt/IngestionPolicy_getIngestionOperationAndParameterDictMQTT.py
@@ -1,138 +1,138 @@
 from DateTime import DateTime
 
-context.log("Hello!")
-
 now = DateTime()
-now_string = now.strftime('%Y%m%d-%H%M%S-%f')[:-3]
-
+today_string = now.strftime('%Y%m%d')
 portal = context.getPortalObject()
 portal_catalog = portal.portal_catalog
-
-reference_separator = portal.ERP5Site_getIngestionReferenceDictionary()["reference_separator"]
-reference_end_single = portal.ERP5Site_getIngestionReferenceDictionary()["single_end_suffix"]
-#none_extension = portal.ERP5Site_getIngestionReferenceDictionary()["none_extension"]
-
-# remove supplier, eof, size and hash from reference
-reference = reference_separator.join(reference.split(reference_separator)[1:-3])
-
-data_ingestion_reference = reference
-eof = movement_dict.get('eof', reference_end_single) if movement_dict.get('eof', reference_end_single) != "" else reference_end_single
+context.log("It is being executed!")
+data_ingestion_reference = movement_dict.get('reference', reference)
 resource_reference = movement_dict.get('resource_reference', None)
-supplier = movement_dict.get('supplier', None)
-#extension = movement_dict.get('extension', None)
-dataset_reference = movement_dict.get('dataset_reference', None)
-data_ingestion_id =  '%s_%s_%s_%s' %(supplier, dataset_reference, now_string, eof)
+specialise_reference = movement_dict.get('specialise_reference', None)
+data_ingestion_id =  "%s-%s" %(today_string, data_ingestion_reference)
 
-# search for applicable data ingestion
-data_ingestion = portal_catalog.getResultValue(
+data_ingestion_query_kw = dict(
   portal_type = 'Data Ingestion',
+  simulation_state = ['started', 'stopped'],
   reference = data_ingestion_reference)
 
-# if data_ingestion is not None:
-  # reject new ingestion with same reference if it arrives during ongoing previous ingestion
-#  if data_ingestion.getSimulationState() == 'started' and eof == reference_end_single:
-#    raise Exception("Wendelin is already processing an ingestion with reference " + reference)
-#  if data_ingestion.getSimulationState() != 'started':
-    # new ingestion replaces existing one (ebulk partial ingestion) unless split
-#    if eof == reference_end_single:
-#      portal.ERP5Site_invalidateIngestionObjects(data_ingestion_reference)
+# first search for applicable data ingestion
+data_ingestion = portal_catalog.getResultValue(**data_ingestion_query_kw)
 
-specialise_value_list = [x.getObject() for x in portal_catalog.searchResults(
-  portal_type = 'Data Supply',
-  reference = 'default_ebulk',
-  validation_state = 'validated')]
+def init_input_line(input_line, operation_line):
+  # copy device and configuration from operation line to input line
+  input_line.setAggregateSet(
+    input_line.getAggregateList() + operation_line.getAggregateList())
 
-# create a new data ingestion
-data_ingestion = portal.data_ingestion_module.newContent(
-    id = data_ingestion_id,
-    portal_type = "Data Ingestion",
-    title = movement_dict.get('filename', data_ingestion_reference),
-    reference = data_ingestion_reference,
-    start_date = now,
-    specialise_value_list = specialise_value_list)
+  # Check if we have a batch referece
+  data_ingestion_batch_reference =  movement_dict.get(
+                                    'aggregate_data_ingestion_batch_reference',
+                                    None)
+  data_ingestion_batch_id =  "%s-%s" %(today_string,
+                                       data_ingestion_batch_reference)
 
-property_list = ["title",
-                 "source",
-                 "source_section",
-                 "source_project",
-                 "destination",
-                 "destination_section",
-                 "destination_project",
-                 "specialise"]
+  data_sink_type_list = []
+  data_sink_list = []
+  data_product = None
+  if data_ingestion_batch_reference is not None:
+    data_ingestion_batch = portal_catalog.getResultValue(
+      portal_type = "Data Ingestion Batch",
+      reference = data_ingestion_batch_reference)
 
-composed = data_ingestion.asComposedDocument()
-data_ingestion.edit(**{p: composed.getProperty(p) for p in property_list})
+    if data_ingestion_batch is None:
+      data_ingestion_batch = portal.data_ingestion_batch_module.get(
+                                                        data_ingestion_batch_id)
+    if data_ingestion_batch is None:
+      data_ingestion_batch = portal.data_ingestion_batch_module.newContent(
+        id = data_ingestion_batch_id,
+        portal_type = "Data Ingestion Batch",
+        reference = data_ingestion_batch_reference)
 
-# create ingestion lines from specialise lines and assign input line
-# and operation line
-#input_line = None
-operation_line = None
-for supply_line in composed.objectValues(portal_type = 'Data Supply Line'):
-  current_line = data_ingestion.newContent(
-    portal_type = "Data Ingestion Line",
-    title = supply_line.getTitle(),
-    aggregate = supply_line.getAggregateList(),
-    int_index = supply_line.getIntIndex(),
-    quantity = supply_line.getQuantity(),
-    reference = supply_line.getReference(),
-    resource = supply_line.getResource(),
-  )
-  if current_line.getResourceReference() == resource_reference:
-    input_line = current_line
-  elif current_line.getResourceValue().getPortalType() == "Data Operation":
-    operation_line = current_line
-  else:
-    # we set quantity=0 for the empty line
-    current_line.setQuantity(0)
+    else:
+      previous_data_ingestion_line = portal_catalog.getResultValue(
+        portal_type = "Data Ingestion Line",
+        resource_reference = resource_reference,
+        aggregate_uid = data_ingestion_batch.getUid())
 
-# copy device and configuration from operation line to input line
-#input_line.setAggregateSet(input_line.getAggregateList() + operation_line.getAggregateList())
+      if previous_data_ingestion_line is not None:
+        data_product = previous_data_ingestion_line.getResourceValue()
+        data_sink_type_list = data_product.getAggregatedPortalTypeList()
+        data_sink_list = previous_data_ingestion_line\
+                         .getAggregateValueList(portal_type=data_sink_type_list)
 
-#data_stream = portal.data_stream_module.newContent(
-#  portal_type = "Data Stream",
-#  id = data_ingestion_id,
-#  title = "%s%s" % (data_ingestion.getTitle(), "."+extension if extension != none_extension else ""),
-#  reference = data_ingestion_reference)
+    input_line.setDefaultAggregateValue(data_ingestion_batch)
 
-#input_line.setDefaultAggregateValue(data_stream)
+  if not data_sink_list:
+    if not data_sink_type_list:
+      if data_product is None:
+        data_product = portal.portal_catalog.getResultValue(
+          portal_type = "Data Product",
+          reference = resource_reference)
+      data_sink_type_list = data_product.getAggregatedPortalTypeList()
 
-#if dataset_reference is not None:
-#  data_set = portal.data_set_module.get(dataset_reference)
-#  try:
-#    if data_set is None:
-#      data_set = portal.data_set_module.newContent(
-#        portal_type = "Data Set",
-#        title = dataset_reference,
-#        reference = dataset_reference,
-#        id = dataset_reference, # XXX: is it really needed ?
-#        version = "000")
-      # when a data set is uploaded from ebulk this means that "validation" is done at client side
-      # thus set set accordingly
-#      data_set.validate()
-#  except Exception:
-#    data_set = portal.data_set_module.get(dataset_reference)
-#  if portal.ERP5Site_checkReferenceInvalidated(data_set):
-#    portal.ERP5Site_revalidateReference(data_set)
-#  if data_set.getValidationState() == "invalidated":
-#    data_set.validate()
-#  input_line.setDefaultAggregateValue(data_set)
+    for data_sink_type in data_sink_type_list:
+      # This should be more generic
+      if data_sink_type not in ("Progress Indicator", "Data Ingestion Batch"):
+        # first try to find existing validated data sink
+        # with same device, project, resource (but can be different source)
+        data_sink = portal.portal_catalog.getResultValue(
+          portal_type=data_sink_type,
+          validation_state="validated",
+          item_device_relative_url=operation_line.getAggregateDevice(),
+          item_project_relative_url=input_line.getDestinationProject(),
+          item_resource_uid=input_line.getResourceUid())
 
-#data_ingestion.start()
+        if data_sink is None:
+          data_sink = portal.getDefaultModule(data_sink_type).newContent(
+            portal_type = data_sink_type,
+            reference = "%s-%s" %(data_ingestion_reference, resource_reference))
+          data_sink.validate()
+        data_sink_list.append(data_sink)
 
-mqtt_message = portal.mqtt_message_module.newContent(
-  portal_type = "MQTT Message",
-  title="test_topic",
-  payload="test_payload"
-)
+  input_line.setAggregateValueList(
+                            input_line.getAggregateValueList() + data_sink_list)
+  input_line.setQuantity(1)
 
-context.log("I am now here ...")
+if data_ingestion is None:
+  document = portal.data_ingestion_module.get(data_ingestion_id)
+  if (document is not None) and document.getSimulationState() in ('started', 'stopped'):
+    data_ingestion = document
 
-# mqtt_message.validate()
+if modify and data_ingestion is None:
+  specialise_query_kw = dict(portal_type = 'Data Supply',
+    reference = specialise_reference,
+    validation_state = 'validated')
+
+  specialise_list = [x.getRelativeUrl() for x in portal_catalog(**specialise_query_kw)]
+
+  # if we do not find a validated data supply, we look for a default data supply
+  if not specialise_list:
+    specialise_query_kw["validation_state"] = 'default'
+    specialise_list = [x.getRelativeUrl() for x in portal_catalog(**specialise_query_kw)]
+
+  # create a new data ingestion
+  data_ingestion = portal.ERP5Site_createDataIngestion(specialise_list,
+                                                       data_ingestion_reference,
+                                                       data_ingestion_id)
+
+
+# find ingestion line for current resource
+for line in data_ingestion.objectValues(portal_type="Data Ingestion Line"):
+  if line.getResourceReference() == resource_reference:
+    input_line = line
+  elif line.getResourceValue().getPortalType() == "Data Operation":
+    operation_line = line
+
+if modify and input_line.getQuantity() == 0:
+  init_input_line(input_line, operation_line)
 
 data_operation = operation_line.getResourceValue()
-#data_stream = input_line.getAggregateDataStreamValue()
+parameter_dict = {
+   input_line.getReference(): \
+     {v.getPortalType(): v for v in input_line.getAggregateValueList()}}
+bucket_reference = movement_dict.get('bucket_reference', None)
+if bucket_reference is not None:
+  parameter_dict['bucket_reference'] = bucket_reference
 
-# if eof == reference_end_single:
-#  data_stream.validate()
-
-return data_operation, {'data_stream': None}
+context.log(data_operation)
+context.log(parameter_dict)
+return data_operation, parameter_dict
