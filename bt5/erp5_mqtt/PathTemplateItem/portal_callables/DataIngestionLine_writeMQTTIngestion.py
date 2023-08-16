171295fa2a1a4cc0494687b6ceae7e7fd3c99129
Martin Manchev <mmanchev23@gmail.com>
2023-08-16 10:54:34 +0200

    Modify Data Ingestion Line Python 2 Script for MQTT
    - Accepts string
    - Serializes the string into json
    - Creates new MQTT Message object with the data
    - Requires 'topic' and 'payload' to be present in the json

--- bt5/erp5_mqtt/PathTemplateItem/portal_callables/DataIngestionLine_writeMQTTIngestion.py
+++ bt5/erp5_mqtt/PathTemplateItem/portal_callables/DataIngestionLine_writeMQTTIngestion.py
@@ -1,9 +1,14 @@
-portal = context.getPortalObject()
+import json
 
-context.log(data_chunk)
+portal = context.getPortalObject()
+data = "".join([str(c[1]) for c in context.unpack(data_chunk)])
+data = data.replace("'", "\"")
+data = json.loads(data)
+title = data["topic"]
+payload = data["payload"]
 
 mqtt_message = portal.mqtt_message_module.newContent(
-  portal_type = "MQTT Message",
-  title="test_topic",
-  payload="test_payload"
+    portal_type="MQTT Message",
+    title=title,
+    payload=payload
 )
