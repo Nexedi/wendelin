1fbc4096260e97e42b198a4dddb31f6ea20830b0
Nikola Balog <nikola.balog@nexedi.com>
2023-09-08 16:18:57 +0900

    Added notebook for visualization of data

--- bt5/erp5_wendelin_drone/PathTemplateItem/portal_callables/fitness_score_drones.py
+++ bt5/erp5_wendelin_drone/PathTemplateItem/portal_callables/fitness_score_drones.py
@@ -46,23 +46,29 @@ def create_scorelist(scores):
 
 
 
-def create_score_dataframe(scores, mean_lists, iteration=0):
-    #df = pd.DataFrame(columns=['Iteration', 'Sim Name', 'Parameters', 'Score', 'Distance mean', 'ASML mean', 'Ground Speed mean', 'Climb Rate mean'])
-    df = pd.DataFrame(columns=['Iteration', 'Parameters1', 'Parameters2', 'Score', 'Distance mean', 'ASML mean', 'Ground Speed mean', 'Climb Rate mean'])
+def create_score_dataframe(scores, mean_lists):
+    df = pd.DataFrame(columns=['Sim_name', 'Parameters1', 'Parameters2', 'Score', 'Distance mean', 'ASML mean', 'Ground Speed mean', 'Climb Rate mean'])
     for score in scores:
-        row=[int(iteration)]
-        row.extend([score[2][0],score[2][1], int(score[3])])
+        row=[]
+        row.extend([score[1],score[2][0],score[2][1], int(score[3])])
         for data in mean_lists:
             for i in data:
-                #print(i)
                 if i[1] == score[0]:
                     row.append(i[0])
                     break
-        context.log(row)
         new_row = pd.DataFrame([row], columns=df.columns)
         df = pd.concat([df, new_row], ignore_index=True)
     return df
 
+def create_plot_dataframe(plot_data):
+  df = pd.DataFrame({
+    "time": plot_data[0],
+    "distance":plot_data[1],
+    "ASML": plot_data[2],
+    "ground_speed": plot_data[3],
+    "climb_rate": plot_data[4]
+    })
+  return df
 
 real_array = input_array_real["Data Array"]
 sim_array = input_array_sim["Data Array"]
@@ -148,7 +154,7 @@ for figure_index, (distance_list_list, limit_tuple) in enumerate((
     mean_list_list.append([])
     sizes = [0, 0, 0, 0]
     # For each line of the analysed sim thing we take the absolute mean of the values (distances, asml, ground speed, climb rate)
-    # the figure index 
+    # the figure index
     for distance_list_index, distance_list in enumerate(distance_list_list):
         mean = mean_operation(map(abs, distance_list))
         mean_list_list[figure_index].append((mean, distance_list_index))
@@ -179,21 +185,13 @@ timestamp_list = simulated_flights_value_dict_list[to_plot_index]["timestamp"]
 score_list2 = create_scorelist(sorted_score_list)
 
 
-
-
 zbigarray = out_array_scores["Data Array"].getArray()
 
-if zbigarray is None:
-  iteration = 1
-else:
-  try:
-    iteration = zbigarray[-1][1] + 1 # Look at the last iteration number
-  except:
-    iteration = 1
+score_names = list(out_array_scores["Data Array"])
 
 
-df = create_score_dataframe(score_list2, mean_list_list, iteration)
-dtypes = {'index': 'i8', 'Iteration': 'i8', 'Parameters1': 'f8', 'Parameters2': 'f8',
+df = create_score_dataframe(score_list2, mean_list_list)
+dtypes = {'index': 'i8','Sim_name':'S256', 'Parameters1': 'f16', 'Parameters2': 'f16',
           'Score': 'f8', 'Distance_mean': 'f8', 'ASML_mean': 'f8',
           'Ground_Speed_mean': 'f8', 'Climb_Rate_mean': 'f8'}
 
@@ -205,9 +203,104 @@ ndarray = df.to_records(column_dtypes=dtypes, index = False)
 if zbigarray is None:
   zbigarray = out_array_scores["Data Array"].initArray(shape=(0,), dtype=ndarray.dtype.fields)
 
+score_array_start_idx = zbigarray.shape[0]
 
 zbigarray.append(ndarray)
 
+
+
+new_key = None
+
+try:
+  max_nr = 0
+  for key in score_names:
+    old_key_nr = int(key.split("_")[1])
+    if old_key_nr>max_nr:
+      max_nr=old_key_nr
+    new_key = "Iteration_" + str(max_nr+1)
+except:
+  new_key = "Iteration_1"
+
+
+
+
+
+data_array_line_score = out_array_scores.get(new_key)
+
+if data_array_line_score is None:
+  data_array_line_score = out_array_scores["Data Array"].newContent(id=new_key,
+                                             portal_type="Data Array Line")
+
+data_array_line_score.edit(reference=new_key,
+     index_expression="%s:%s" %(score_array_start_idx, zbigarray.shape[0])
+  )
+
+
+
+zbigarray2 = out_array_plot_data["Data Array"].getArray()
+#Uncomment this to set the array to empty
+#out_array_plot_data["Data Array"] = out_array_plot_data["Data Array"].initArray(shape=(0,), dtype=ndarray.dtype.fields)
+
+things = [timestamp_list,
+    simulated_flights_value_dict_list[to_plot_index]["distance"],
+    simulated_flights_value_dict_list[to_plot_index]["ASML"],
+    simulated_flights_value_dict_list[to_plot_index]["ground speed"],
+    simulated_flights_value_dict_list[to_plot_index]["climb rate"]]
+
+
+
+
+
+
+
+df = create_plot_dataframe(things)
+dtypes = {'time': 'f8', 'distance': 'f8', 'ASML': 'f8',
+          'ground_speed': 'f8', 'climb_rate': 'f8'}
+
+
+ndarray2 = df.to_records(column_dtypes=dtypes, index = False)
+context.log(ndarray2)
+if zbigarray2 is None:
+  context.log("zbigarray2 is none")
+  zbigarray2 = out_array_plot_data["Data Array"].initArray(shape=(0,), dtype=ndarray2.dtype.fields)
+
+
+plot_array_start_idx = zbigarray2.shape[0]
+
+zbigarray2.append(ndarray2)
+
+
+
+new_key_plot = "PlotIteration_1"
+plot_names = list(out_array_plot_data["Data Array"])
+context.log(plot_names)
+try:
+  context.log("OK")
+  max_nr = 0
+  for key in plot_names:
+    old_key_nr = int(key.split("_")[1])
+    if old_key_nr>max_nr:
+      max_nr=old_key_nr
+    new_key_plot = "PlotIteration_" + str(max_nr+1)
+    
+except:
+  context.log("h")
+  new_key_plot = "PlotIteration_1"
+
+context.log(new_key_plot)
+
+
+
+
+data_array_line_plot = out_array_plot_data.get(new_key_plot)
+
+if data_array_line_plot is None:
+  data_array_line_plot = out_array_plot_data["Data Array"].newContent(id=new_key_plot,
+                                             portal_type="Data Array Line")
+
+data_array_line_plot.edit(reference=new_key_plot,
+     index_expression="%s:%s" %(plot_array_start_idx, zbigarray2.shape[0])
+  )
 
 if end > start:
   progress_indicator_sim.setIntOffsetIndex(end)
