acb645a5e5cec8330e8551868c4442334e610461
Paul Graydon <paul.graydon@nexedi.com>
2024-11-22 14:32:04 +0000

    erp5_wendelin: Fix DataAnalysis_clearAndReprocessFromScratch

--- bt5/erp5_wendelin/SkinTemplateItem/portal_skins/erp5_wendelin/DataAnalysis_clearAndReprocessFromScratch.py
+++ bt5/erp5_wendelin/SkinTemplateItem/portal_skins/erp5_wendelin/DataAnalysis_clearAndReprocessFromScratch.py
@@ -13,7 +13,6 @@ for line in context.objectValues(portal_type="Data Analysis Line"):
     data_array = line.getAggregateDataArrayValue()
     if data_array is not None:
       data_array.setArray(None)
-      data_array.setStartDate(None)
       data_array.manage_delObjects(list(data_array.keys()))
 
 if context.getRefreshState() != "refresh_started":
