7b330649c224f30a48bc57652329ad74537b85fb
Paul Graydon <paul.graydon@nexedi.com>
2025-04-24 09:42:06 +0000

    erp5_wendelin_telecom_security: Update security interaction workflow scripts
    - Update activity tags to be more explicit
    - Fix variable naming
    - Remove useless error handling

--- bt5/erp5_wendelin_telecom_security/WorkflowTemplateItem/portal_workflow/wendelin_telecom_security_interaction_workflow/script_DataAnalysis_updateLineRelatedItemLocalRoles.py
+++ bt5/erp5_wendelin_telecom_security/WorkflowTemplateItem/portal_workflow/wendelin_telecom_security_interaction_workflow/script_DataAnalysis_updateLineRelatedItemLocalRoles.py
@@ -1,20 +1,18 @@
-tag = script.id
+data_analysis = state_change['object']
+
+tag = script.id + '_' + data_analysis.getRelativeUrl()
 activate_kw = dict(tag=tag)
 
-data_analysis = state_change['object']
 data_analysis.reindexObject(activate_kw=activate_kw)
 
-data_analysis_lines = data_analysis.contentValues(portal_type='Data Analysis Line')
-if len(data_analysis_lines) == 0:
+data_analysis_line_list = data_analysis.contentValues(portal_type='Data Analysis Line')
+if len(data_analysis_line_list) == 0:
   return
 
-for data_analysis_line in data_analysis_lines:
+for data_analysis_line in data_analysis_line_list:
   data_analysis_line.activate(after_tag=tag).updateLocalRolesOnSecurityGroups()
   for value in data_analysis_line.getAggregateValueList():
     value.activate(after_tag=tag).updateLocalRolesOnSecurityGroups()
-    try:
-      content_values = value.contentValues()
-      for content_value in content_values:
-        content_value.activate(after_tag=tag).updateLocalRolesOnSecurityGroups()
-    except AttributeError:
-      pass
+    content_value_list = value.contentValues()
+    for content_value in content_value_list:
+      content_value.activate(after_tag=tag).updateLocalRolesOnSecurityGroups()
--- bt5/erp5_wendelin_telecom_security/WorkflowTemplateItem/portal_workflow/wendelin_telecom_security_interaction_workflow/script_DataSupply_updateLineAggregateLocalRoles.py
+++ bt5/erp5_wendelin_telecom_security/WorkflowTemplateItem/portal_workflow/wendelin_telecom_security_interaction_workflow/script_DataSupply_updateLineAggregateLocalRoles.py
@@ -1,14 +1,15 @@
-tag = script.id
+data_supply = state_change['object']
+
+tag = script.id + '_' + data_supply.getRelativeUrl()
 activate_kw = dict(tag=tag)
 
-data_supply = state_change['object']
 data_supply.reindexObject(activate_kw=activate_kw)
 
-data_supply_lines = data_supply.contentValues(portal_type='Data Supply Line')
-if len(data_supply_lines) == 0:
+data_supply_line_list = data_supply.contentValues(portal_type='Data Supply Line')
+if len(data_supply_line_list) == 0:
   return
 
-for data_supply_line in data_supply_lines:
+for data_supply_line in data_supply_line_list:
   data_supply_line.activate(after_tag=tag).updateLocalRolesOnSecurityGroups()
   for value in data_supply_line.getAggregateValueList():
     value.activate(after_tag=tag).updateLocalRolesOnSecurityGroups()
