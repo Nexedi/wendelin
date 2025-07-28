4f15fa95b613a596cb17ca24f23e26adada3c845
Xiaowu Zhang <xiaowu.zhang@nexedi.com>
2025-07-28 14:21:35 +0200

    *: move wendelin to erp5

--- bt5/erp5_wendelin/SkinTemplateItem/portal_skins/erp5_wendelin/DataSet_getDataStreamCount.py
+++
@@ -1,4 +0,0 @@
-"""
-Get count of Data Streams for context Data set.
-"""
-return context.ERP5Site_getDataStreamCount(data_set_reference=context.getReference())['result']
--- bt5/erp5_wendelin/SkinTemplateItem/portal_skins/erp5_wendelin/DataStream_appendFile.py
+++
@@ -1,7 +0,0 @@
-context.appendData(append_file.read())
-  
-message = context.Base_translateString("Data Stream updated.")
-return context.Base_redirect(
-  'view', 
-  keep_items=dict(portal_status_message=message)
-)
